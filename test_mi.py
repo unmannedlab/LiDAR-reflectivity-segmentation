256c20a7bf3a11f0cb648555e1f7c221637e6860
kasiv008 <kasiv008@gmail.com>
2024-03-20 13:21:06 -0500

    delete

--- test_mi.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchmetrics.clustering import MutualInfoScore
-input = torch.tensor([2, 1, 0, 1, 0],dtype=torch.float32)
-target = torch.tensor([0, 2, 1, 1, 0],dtype=torch.float32)
-weights = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5],requires_grad=True,dtype=torch.float32)
-preds = input*weights
-mi_score = MutualInfoScore()
-mi_loss = mi_score(preds, target)
-mi_loss.backward()
