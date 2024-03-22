ab39970583b5870ce107813d70bb966dce061e35
kasiv008 <kasiv008@gmail.com>
2024-03-22 15:55:07 -0500

    path change

--- run_salsanext0.sh
+++ run_salsanext0.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export CUDA_VISIBLE_DEVICES="0"
-cd ./train/tasks/semantic;  ./train.py -d /media/usl/Data/Dataset/Semantic-poss/semantic_poss_reflectivity/sequences/  -ac ./config/arch/poss/salsanext_rxyzi.yml -dc ./config/labels/poss.yaml -n poss_rxyzi -l ./logs -p ""
+cd ./train/tasks/semantic;  ./train.py -d path/to/dataset/rellid_lidar_nr/  -ac ./config/arch/rellis/salsanext_rxyzi.yml -dc ./config/labels/rellis.yaml -n rellis_rxyzi -l ./logs -p ""
--- run_salsanext1.sh
+++ run_salsanext1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export CUDA_VISIBLE_DEVICES="1"
-cd ./train/tasks/semantic;  ./train.py -d /media/peng/7c3e5329-39d0-4b7d-a8fe-af5ad9c0d370/Datasets/data_reflectivity_velodyne_v2/dataset/sequences/  -ac ./config/arch/kitti/salsanext_rxyzi.yml -dc ./config/labels/semantic-kitti.yaml -n kitti_rxyzi -l ./logs -p ""
+cd ./train/tasks/semantic;  ./train.py -d /path/to/dataset/rellis_lidar_nr  -ac ./config/arch/rellis/salsanext_rxyzn.yml -dc ./config/labels/rellis.yaml -n rellis_rxyzn -l ./logs -p ""
